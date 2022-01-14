12f00b510cbc326675c6b018251a44e8fdbe9d6a
Romero, Matias (partner) <matias.romero@radventure.com>
2022-01-14 20:31:16 -0300

    courses

---
+++ js-repaso/fatfn.js
@@ -0,0 +1,8 @@
+function Fn() {
+    this.nombre = 'nombre'
+    return 'chanchito feliz'
+}
+
+const r = new Fn()
+
+console.log(r)
---
+++ js-repaso/variables.js
@@ -0,0 +1,7 @@
+console.log(myVar)
+
+var myVar = 'variable'
+
+let variable = 'variable con let'
+
+const constante = 'este valor es constante'
