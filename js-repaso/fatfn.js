843654a62da5b1e6331de86e6d3e07116b2c16bd
Romero, Matias (partner) <matias.romero@radventure.com>
2022-01-16 21:12:10 -0300

    shopcart - initial commit

--- js-repaso/fatfn.js
+++
@@ -1,8 +0,0 @@
-function Fn() {
-    this.nombre = 'nombre'
-    return 'chanchito feliz'
-}
-
-const r = new Fn()
-
-console.log(r)
--- js-repaso/variables.js
+++
@@ -1,7 +0,0 @@
-console.log(myVar)
-
-var myVar = 'variable'
-
-let variable = 'variable con let'
-
-const constante = 'este valor es constante'
