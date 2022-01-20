2c58980c3a6241fd66125c1084a621d94af5d61c
Romero, Matias (partner) <matias.romero@radventure.com>
2022-01-20 12:25:09 -0300

    functional components part 2

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,9 +1,22 @@
-const MyComponent = ({ myProp }) => {
-  return <div>Nombre: {myProp}</div>;
-};
+import { useState } from "react";
 
+// rules for hooks:
+//  can't be called inside a loop (if, while, else, foreach)
+//  only called in two steps (components / custom hooks)
+const styles = {
+  counter: {
+    margin: "0 20 px",
+  },
+};
 const App = () => {
-  return <MyComponent myProp={"hola"} />;
+  const [counter, setCounter] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setCounter(counter - 1)}> - </button>
+      <span style={styles.counter}>Counter: {counter}</span>
+      <button onClick={() => setCounter(counter + 1)}> + </button>
+    </div>
+  );
 };
 
 export default App;
