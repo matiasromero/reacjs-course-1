4e13f476824a602eedcbbac8d04ada758ab460d3
Romero, Matias (partner) <matias.romero@radventure.com>
2022-02-28 14:34:31 -0300

    useRef

---
+++ functional-components/src/useRef.js
@@ -0,0 +1,23 @@
+import { useRef } from "react";
+
+const App = () => {
+  const ref = useRef();
+  const inputRef = useRef();
+  const click = () => {
+    console.log(ref.current.clientHeight);
+  };
+  const focus = () => {
+    inputRef.current.focus();
+  };
+  return (
+    <div>
+      <input ref={inputRef} />
+      <button onClick={focus}>Focus!</button>
+      <div onClick={click} ref={ref}>
+        LALA
+      </div>
+    </div>
+  );
+};
+
+export default App;
