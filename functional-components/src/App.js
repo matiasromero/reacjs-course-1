2adabadf43749adaea85afdce8924087742b07ba
Romero, Matias (partner) <matias.romero@radventure.com>
2022-03-09 12:10:31 -0300

    inputs, checkbox, radio, select

--- functional-components/src/App.js
+++ functional-components/src/App.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 const App = () => {
-  const [value, setValue] = useState({ normal: "Nombre", text: "Descripcion" });
-  const handleChange = (e) => {
-    setValue({
-      ...value,
-      [e.target.name]: e.target.value,
-    });
+  const [value, setValue] = useState({
+    normal: "Nombre",
+    text: "Descripcion",
+    select: "",
+    check: false,
+    state: "sad",
+  });
+  const handleChange = ({ target }) => {
+    setValue((state) => ({
+      ...state,
+      [target.name]: target.type === "checkbox" ? target.checked : target.value,
+    }));
   };
   console.log(value);
   return (
@@ -18,6 +24,48 @@ const App = () => {
         onChange={handleChange}
       />
       <textarea name="text" onChange={handleChange} value={value.text} />
+
+      <select value={value.select} name="select" onChange={handleChange}>
+        <option value="">-- Select --</option>
+        <option value="happy">Happy</option>
+        <option value="sad">Sad</option>
+        <option value="angry">Angry</option>
+      </select>
+
+      <input
+        type="checkbox"
+        name="check"
+        onChange={handleChange}
+        checked={value.check}
+      />
+
+      <div>
+        <label>Type:</label>
+        <input
+          name="state"
+          type="radio"
+          value="happy"
+          onChange={handleChange}
+          checked={value.state === "happy"}
+        />{" "}
+        Happy
+        <input
+          name="state"
+          type="radio"
+          value="sad"
+          onChange={handleChange}
+          checked={value.state === "sad"}
+        />{" "}
+        Sad
+        <input
+          name="state"
+          type="radio"
+          value="angry"
+          onChange={handleChange}
+          checked={value.state === "angry"}
+        />{" "}
+        Angry
+      </div>
     </div>
   );
 };
